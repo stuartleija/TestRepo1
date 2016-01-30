87efbbcae61af5c42aac8a9c278ee1c72948aa45
DAVITA\sleija <stuart.leija@davita.com>
2016-01-29 17:23:58 -0700

    added file to branch1.2

---
+++ Files/Class3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Files
+{
+    class Class3
+    {
+        //add this to branch1.2, the child of branch1
+    }
+}
